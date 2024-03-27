06050191893226adcf1f6d73ae8ef44096a9e2eb
dangerbaldo <daniele.gerbaldo@gmail.com>
2024-03-27 23:07:39 +0100

    petName existance check

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -135,7 +135,7 @@ class PetController {
 
 		String petName = pet.getName();
 
-		// checking if the pet name already exist for the owner
+		//check pet name existance
 		if (StringUtils.hasText(petName)) {
 			Pet existingPet = owner.getPet(petName.toLowerCase(), false);
 			if (existingPet != null && existingPet.getId() != pet.getId()) {
